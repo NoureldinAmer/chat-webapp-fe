83dc2e7c17e2fb80159f1343ae8d7190f6f4f3c0
Nour Amer <84950398+NoureldinAmer@users.noreply.github.com>
2023-04-21 09:18:43 -0600

    implement link components

---
+++ src/Components/Link/Incoming.js
@@ -0,0 +1,83 @@
+import React from "react";
+import { Avatar, Box, Slide, Stack, Typography } from "@mui/material";
+import Embed from "react-embed";
+import SVG from "react-inlinesvg";
+import { formatDistanceToNowStrict } from "date-fns";
+
+export function IncomingLink(props) {
+  //console.log(props.elm);
+  return (
+    <>
+    <Slide in={true} timeout={props.slideEnabled ? 300 : 0} direction={"up"}>
+    <Stack direction={"column"} key={props.key}>
+      <Stack
+          direction="row"
+          justifyContent="start"
+          alignItems={"center"}
+          display={props.shouldHide ? "none" : null}
+          spacing={0.5}
+        >
+          <Avatar
+            sx={{
+              m: 1,
+              width: "20px",
+              height: "20px",
+              bgcolor: "rgba(256,256,256, 1)",
+            }}
+          >
+            <SVG src={props.avatarSVG} />
+          </Avatar>
+          <Typography variant="caption" fontWeight={550}>
+            {props.elm.msgOwner}
+          </Typography>
+          <Typography variant="caption" fontSize={21} fontWeight={900}>
+            &#183;
+          </Typography>
+          <Typography variant="caption" color={"#8A898E"}>
+            {Date.now() - props.date < 60
+              ? "now"
+              : formatDistanceToNowStrict(props.date, {
+                  addSuffix: true,
+                })}
+          </Typography>
+        </Stack>
+
+      <Stack direction="row" justifyContent="start">
+        <Box
+          px={1.5}
+          py={1.5}
+          sx={{
+            bgcolor: "lightgrey",
+            borderRadius: 1.5,
+            width: "max-content",
+            maxWidth: "60%",
+          }}
+        >
+          <Stack spacing={2}>
+            <Stack
+              direction="column"
+              alignItems="center"
+              sx={{
+                borderRadius: 1,
+              }}
+            >
+              <Embed isDark url={props.elm.link} />
+            </Stack>
+            <Typography
+              sx={{
+                color: "black",
+                whiteSpace: "pre-wrap",
+                wordWrap: "break-word",
+              }}
+              fontFamily={"SF pro"}
+            >
+              {props.elm.message}
+            </Typography>
+          </Stack>
+        </Box>
+      </Stack>
+    </Stack>
+    </Slide>
+    </>
+  );
+}
